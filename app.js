2adfb1d744c2fe0f11cc03ae2ad633947d572ce1
MD. Sakib Hossain <dollarjunior03@gmail.com>
2018-05-05 23:25:01 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+
+//Callback funciton is when we pass an function as an argument in another function 
+
+var userName = [
+    'sakib', 'Zannat', 'Anything'
+]
+
+//Print is a callback function
+let print = function(){
+    console.log(userName);
+}
+
+//We pass print() function as an argument
+function addUserName(Name, print){
+    userName.push(Name);
+    print();
+}
+    
+//Here we use the callback function
+addUserName('newName', print);
+addUserName('AnotherNewName', print);
+
